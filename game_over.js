d41ab031321c279d6fb52a57d680945e6ec0fd4f
DarkShrill <edo.dicembre@gmail.com>
2021-08-02 17:42:24 +0200

    - Added files

---
+++ game_over.js
@@ -0,0 +1,45 @@
+import * as THREE_AUDIO from '../libs/threejs/build/three.module.js';
+
+
+var sound_to_play;  
+document.getElementById('game_over_button').onclick = function(){
+    window.location.href = window.location.href.replace("game_over.html","");
+};
+
+
+var listener = new THREE_AUDIO.AudioListener();
+const sounds = {
+    background  :  { url: './asserts/sounds/game_over.mp3' }
+}
+
+
+const soundsLoaderManager = new THREE_AUDIO.LoadingManager();
+soundsLoaderManager.onLoad = () => {
+
+};
+
+soundsLoaderManager.onProgress = (url, itemsLoaded, itemsTotal) => {
+    console.log("Loading sounds... ", itemsLoaded / itemsTotal * 100, '%');
+};
+{
+    const audioLoader = new THREE_AUDIO.AudioLoader(soundsLoaderManager);
+    for (const sound of Object.values(sounds)) {
+        audioLoader.load( sound.url, function( buffer ) {
+            
+            sound.sound = buffer;
+
+            console.log("Loaded ", buffer);
+            console.log("SOUND LOADED!");
+            sound_to_play = new THREE_AUDIO.Audio(listener);
+            sound_to_play.isPlaying = false;
+            sound_to_play.setBuffer( sounds.background.sound );
+            sound_to_play.setLoop( true );
+            sound_to_play.setVolume( 0.3 );
+
+            console.log("sound_to_play.play();");
+            sound_to_play.play();
+
+
+        });
+    }
+} 
